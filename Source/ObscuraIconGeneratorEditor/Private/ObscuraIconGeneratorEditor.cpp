8fab54666b433a853a28d1c8470bb04baff7b23d
David Borsodi <hello@borsodidavid.com>
2025-06-28 11:07:34 +0200

    feat/main: adding Dynamic "Open Icon Generator" menu to supported class of assets

--- Source/ObscuraIconGeneratorEditor/Private/ObscuraIconGeneratorEditor.cpp
+++ Source/ObscuraIconGeneratorEditor/Private/ObscuraIconGeneratorEditor.cpp
@@ -5,6 +5,7 @@
 #include "ContentBrowserMenuContexts.h"
 #include "AssetRegistry/AssetRegistryModule.h"
 #include "DeveloperSettings/ObscuraDeveloperSettings.h"
+#include "Kismet/KismetMathLibrary.h"
 
 #define LOCTEXT_NAMESPACE "FObscuraIconGeneratorEditorModule"
 
@@ -24,54 +25,109 @@ void FObscuraIconGeneratorEditorModule::ShutdownModule()
 
 void FObscuraIconGeneratorEditorModule::RegisterMenus()
 {
-	if (const UObscuraDeveloperSettings* ObscuraDeveloperSettings = GetDefault<UObscuraDeveloperSettings>()) {
-		TArray<TSoftClassPtr<UObject>> SupportedClasses = ObscuraDeveloperSettings->GetSupportedClasses();
+	UToolMenu* Menu = UToolMenus::Get()->ExtendMenu("ContentBrowser.AssetContextMenu");
 
-		UToolMenu* Menu = UToolMenus::Get()->ExtendMenu("ContentBrowser.AssetContextMenu");
-		//TODO: Figure out how to make this conditional 
-		
-		//Menu->Context
-		Menu->AddDynamicSection("ObscuraIconGenerator", FNewToolMenuDelegate::CreateLambda([](UToolMenu* InMenu){
-			// Maybe try to get asset type from context?
-		}),
-		FToolMenuInsert("AssetContextExploreMenuOptions", EToolMenuInsertType::Before));
-		
-		FToolMenuSection& Section = Menu->AddSection(
-			"ObscuraIconGenerator",
-			FText::FromString("Obscura"),
-			FToolMenuInsert("AssetContextExploreMenuOptions", EToolMenuInsertType::Before)
-		);
+	FToolMenuSection& Section = Menu->AddSection(
+		"ObscuraIconGenerator",
+		FText::FromString("Obscura"),
+		FToolMenuInsert("AssetContextExploreMenuOptions", EToolMenuInsertType::Before)
+	);
 
-		Section.AddMenuEntry(
-			"OpenIconGenerator",
-			LOCTEXT("OpenIconGenerator", "Open Icon Generator"),
-			LOCTEXT("OpenIconGeneratorTooltip", "Opens the Icon Generator for this Asset"),
-			FSlateIcon(FAppStyle::Get().GetStyleSetName(), "Icons.PlusCircle"),
-			FUIAction()
-		);
-	
-		UContentBrowserAssetContextMenuContext* Context = Section.FindContext<UContentBrowserAssetContextMenuContext>();
-		
-	
-		/*FToolMenuEntry& Entry = Section.AddDynamicEntry("AssetManagerEditorViewCommands",
-			FNewToolMenuSectionDelegate::CreateLambda([](FToolMenuSection& InSection){
-				UContentBrowserAssetContextMenuContext* Context = InSection.FindContext<UContentBrowserAssetContextMenuContext>();
-				if (Context) {
-					FToolUIActionChoice LockAction(FExecuteAction::CreateLambda([Context]() {
-						FAssetRegistryModule& AssetRegistryModule = FModuleManager::Get().LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
-	                
-						for(const FAssetData& SelectedAsset : Context->SelectedAssets)
-						{
+	/*if (const UContentBrowserAssetContextMenuContext* Context = Menu->FindContext<UContentBrowserAssetContextMenuContext>()) {
+			if (const UClass* SelectedAssetClass = Context->CommonClass) {
+				if (SupportedClasses.Find(SelectedAssetClass) != INDEX_NONE) {
+
+					UE_LOG(LogTemp, Warning, TEXT("Supported class: %s"), *GetNameSafe(SelectedAssetClass));
+					
+					const FToolUIActionChoice OpenIconGeneratorAction(FExecuteAction::CreateLambda([Context]() {
+													
+						UE_LOG(LogTemp, Warning, TEXT("Open Icon Generator"));
 						
-						}
-				}));
-
-				InSection.AddEntry(FToolMenuEntry::InitMenuEntry(FName("Lock"), FText::FromString("Lock Export"),
-					FText::FromString("Lock this asset. Can't export if locked."), FSlateIcon(NAME_None, "MyIconName"), LockAction));
+					}));
+					
+					Menu->AddMenuEntry("ObscuraIconGenerator", FToolMenuEntry::InitMenuEntry(
+							FName("Obscura_OpenIconGenerator"),FText::FromString("Open Icon Generator"),
+							FText::FromString("Opens the Icon Generator for this asset."),
+							FSlateIcon(FAppStyle::Get().GetStyleSetName(), "Icons.PlusCircle"), OpenIconGeneratorAction));
+				}
 			}
-		}));*/
-	}
+		}*/
+		
+	Menu->AddDynamicSection("ObscuraIconGenerator_Dynamic", FNewToolMenuDelegate::CreateLambda([](UToolMenu* InMenu){
+		// Getting the Developer Settings here, because this way the user won't need to restart the editor for the SupportedClasses change to be valid
+		if (const UObscuraDeveloperSettings* ObscuraDeveloperSettings = GetDefault<UObscuraDeveloperSettings>()) {
+			TArray<TSoftClassPtr<UObject>> SupportedClasses = ObscuraDeveloperSettings->GetSupportedClasses();
+			
+			if (const UContentBrowserAssetContextMenuContext* Context = InMenu->FindContext<UContentBrowserAssetContextMenuContext>()) {
+				// Getting the class of the selected asset
+				if (UClass* SelectedAssetClass = Context->CommonClass) {
+
+					// Looping Through the SupportedClasses 
+					for (const TSoftClassPtr<UObject>& SupportedClass : SupportedClasses) {
+						// If the select asset's class is a child of the SupportedClass
+						if (UKismetMathLibrary::ClassIsChildOf(SelectedAssetClass,SupportedClass.Get())) {
+
+							// Menu Entry Action
+							const FToolUIActionChoice OpenIconGeneratorAction(FExecuteAction::CreateLambda([Context]() {
+								UE_LOG(LogTemp, Warning, TEXT("Open Icon Generator"));
+							}));
+
+							// Adding the Menu Entry
+							InMenu->AddMenuEntry("ObscuraIconGenerator", FToolMenuEntry::InitMenuEntry(
+								FName("Obscura_OpenIconGenerator"),FText::FromString("Open Icon Generator"),
+								FText::FromString("Opens the Icon Generator for this asset."),
+								FSlateIcon(FAppStyle::Get().GetStyleSetName(), "Icons.PlusCircle"), OpenIconGeneratorAction)
+							);
+							// We break out of the loop
+							break;
+							
+						}						
+					}
+					/*TSoftClassPtr<UObject> FoundClass = SupportedClasses.FindByPredicate(
+						[SelectedAssetClass](const TSoftClassPtr<UObject>& SupportedClass) {
+								return SelectedAssetClass->StaticClass()->IsInA(SupportedClass.Get());
+							}
+					);*/
+					
+				}
+			}
+		}
+		
+		
+		
+		// Maybe try to get asset type from context?
+	}),
+	FToolMenuInsert("AssetContextExploreMenuOptions", EToolMenuInsertType::Before));
 	
+	/*FToolMenuSection& Section = Menu->AddSection(
+		"ObscuraIconGenerator",
+		FText::FromString("Obscura"),
+		FToolMenuInsert("AssetContextExploreMenuOptions", EToolMenuInsertType::Before)
+	);*/
+
+	
+
+	UContentBrowserAssetContextMenuContext* Context = Section.FindContext<UContentBrowserAssetContextMenuContext>();
+	
+
+	/*FToolMenuEntry& Entry = Section.AddDynamicEntry("AssetManagerEditorViewCommands",
+		FNewToolMenuSectionDelegate::CreateLambda([](FToolMenuSection& InSection){
+			UContentBrowserAssetContextMenuContext* Context = InSection.FindContext<UContentBrowserAssetContextMenuContext>();
+			if (Context) {
+				FToolUIActionChoice LockAction(FExecuteAction::CreateLambda([Context]() {
+					FAssetRegistryModule& AssetRegistryModule = FModuleManager::Get().LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+                
+					for(const FAssetData& SelectedAsset : Context->SelectedAssets)
+					{
+					
+					}
+			}));
+
+			InSection.AddEntry(FToolMenuEntry::InitMenuEntry(FName("Lock"), FText::FromString("Lock Export"),
+				FText::FromString("Lock this asset. Can't export if locked."), FSlateIcon(NAME_None, "MyIconName"), LockAction));
+		}
+	}));*/
+
 	
 }
 
